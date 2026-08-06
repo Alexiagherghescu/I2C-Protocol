3bfccda86438de393757c1ed52a87d37ff0f3b54
Alexiagherghescu <160532682+Alexiagherghescu@users.noreply.github.com>
2026-08-06 09:21:38 +0300

    Add files via upload

---
+++ temp_decoder.sv
@@ -0,0 +1,36 @@
+module ctrl_senzor(
+    input logic [15:0] data_out,
+    input logic clock, reset,
+    output logic sign,
+    output logic [11:0] ctrl_senzor_temp
+);
+
+logic [12:0] temp;
+
+always @(posedge clock)
+begin
+    if(reset==1)
+        begin
+        ctrl_senzor_temp<=0;
+        sign<=1'b0;
+        end
+    else 
+        begin
+        
+        sign<=data_out[15];
+        temp<=data_out[15:3];
+        
+        if(data_out[15]==0)
+            begin
+                ctrl_senzor_temp<=data_out[15:3]>>4;   
+            end
+        else 
+            begin
+                ctrl_senzor_temp<=(data_out[15:3]-13'h1000)>>4; 
+            end
+         
+        end
+     
+
+end
+endmodule
---
+++ top.sv
@@ -0,0 +1,117 @@
+module top
+(
+    input logic rx , reset, clock, up_button, down_button, reset_button,
+    inout logic sda,
+    output logic scl, tx, led_sign,
+    output logic [7:0] segmente,
+    output logic [4:0] digits
+);
+
+localparam addr_slave= 'h4B;
+localparam addr_reg= 'h00;
+localparam Celsius='d10;
+
+wire en_i2c, done_tick;
+wire [15:0] data_out;
+wire [11:0] ctrl_senzor_temp;
+wire [1:0] sel_mux;
+wire [3:0] out_mux;
+
+wire [3:0] w_zeci, w_unit, w_zecimala;
+wire [11:0] temp_converted;
+assign temp_converted= {w_zeci, w_unit, w_zecimala};
+
+
+UART_logger_interactiv UART_logger_interactiv
+(
+    .rx(rx),
+    .up_button(up_button), 
+    .down_button(down_button), 
+    .reset_button(reset_button),
+    .clock(clock),
+    .reset(reset),
+    .temp_converted(temp_converted),
+    .tx(tx)
+);
+
+i2c_master i2c_master         
+(    
+    .clock(clock), 
+    .reset(reset), 
+    .en(en_i2c), 
+    .done_tick(done_tick),
+    .data_out(data_out),  
+    .addr_slave(addr_slave),
+    .addr_reg(addr_reg),
+    .scl(scl),
+    .rw(), 
+    .sda(sda) 
+);
+
+
+
+reading_timer #(.clock_freq(100000000),
+                .reading_time_per_sec(2))
+reading_timer
+(
+    .clock(clock), 
+    .reset(reset),
+    .done(en_i2c)
+);
+
+
+
+clock_divider #(.CLOCK_FREQ(1000000),
+                .I2C_FREQ (100000),
+                .sample(4))
+clock_divider
+(
+    .clock(clock), 
+    .reset(reset),
+    .done_tick(done_tick)
+);
+
+ctrl_senzor(
+   .data_out(data_out),
+   .clock(clock), 
+   .reset(reset),
+   .sign(led_sign),
+   .ctrl_senzor_temp(ctrl_senzor_temp)
+);
+
+bcd_temp(
+    .ctrl_senzor_temp(ctrl_senzor_temp),
+    .zeci(w_zeci), 
+    .unit(w_unit), 
+    .zecimala(w_zecimala)
+);
+
+timer timer_mux
+(
+    .clock(clock),
+    .sel(sel_mux)
+);
+
+mux mux
+(
+    .sel(sel_mux),
+    .unit(w_unit), 
+    .zeci(w_zeci), 
+    .C(Celsius),
+    .zecimala(w_zecimala),
+    . out(out_mux)
+);
+
+seg7 seg7
+(
+    .in(out_mux),
+    .segmente(segmente)
+);
+
+
+cifra_sel cifra_sel
+(
+    .in(sel_mux),
+    .out(digits)
+);
+endmodule
